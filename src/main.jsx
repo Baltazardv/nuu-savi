e9af954004fc9fd2a752118a646062eba47a7a24
Jose Juan Martínez <al221910301@gmail.com>
2026-08-27 18:26:11 -0600

    Router basename para subcarpeta (arregla intro y navegacion en GitHub Pages)

--- src/main.jsx
+++ src/main.jsx
@@ -4,9 +4,13 @@ import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
 import "./styles.css";
 
+// basename para que el enrutador funcione en subcarpeta (GitHub Pages: /nuu-savi/)
+// y en la raíz (cPanel: "/").
+const basename = import.meta.env.BASE_URL.replace(/\/$/, "") || "/";
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <BrowserRouter>
+    <BrowserRouter basename={basename}>
       <App />
     </BrowserRouter>
   </React.StrictMode>
